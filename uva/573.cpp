9851b53547a6a9128a8a0cb0723eb677446c8fa2
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2014-09-07 15:30:15 +0800

    update 573 for uva

--- uva/573.cpp
+++ uva/573.cpp
@@ -3,15 +3,31 @@
 
 using namespace std;
 
-#ifndef DEBUG
-#define CIN cin
-#define COUT cout
-#else
+
+#define FLOAT_EQ(x,v) (((v - EPSILON) < x) && (x <( v + EPSILON)))
+
+#define DEUG
+#ifdef DEBUG
+
 ifstream in;
 ofstream out;
 #define CIN in
 #define COUT out
+int init_debug(void){
+	in.open("./in", ios::in);
+	out.open("./out", ios::out);
+	if(in.fail() && out.fail()){
+		return -1;
+	}
+	return 0;
+}
+
+#else
+
+#define CIN cin
+#define COUT cout
 #endif
+
 
 
 
@@ -19,25 +35,44 @@ ofstream out;
 
 int main()
 {
-    double h, u, d, f, mv;
+#ifdef DEBUG
+	init_debug();
+#endif
+    double h, u, d, f;
     while(CIN >> h >> u >> d >> f && h){
-    		f = 1/f;
+    		f = u*f/100.0;
+		double curr = 0.0;
     		int tot_day = 1;
-    		while(u - d >= 0.0 || h > 0.0){
-			 h -= (u - d);
-		 	 u *= f;
+    		while(1){
+#ifdef DEBUG
+	    		cout << " curr = " << curr << " h =" << h << " u=" << u << " d= " << d << " f= ";
+		       	cout << f << " day= " << tot_day<< endl;
+#endif
+			 curr += u;
+			 if(curr > h){
+			    break;
+			 }
+
+			 curr -= d;
+
+			 if(curr <  0.0)
+				 break;
+		 	 
+			 u = u < f ? 0.0 : u - f;
+
 	 		 tot_day++;		 
 		}
 		/**output format rebuild*/
-		if(u - d >= 0.0){
-			COUT << "success ";
+		if(curr >= h){
+			COUT << "success on day ";
 		}else{
-			COUT << "fail";
+			COUT << "failure on day ";
 		}
 		COUT << tot_day << endl;
-		
+#ifdef DEBUG
+		cout << endl;
+#endif	
     }
-
     return 0;
 }
 
