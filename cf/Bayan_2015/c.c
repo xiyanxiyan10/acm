f881aa222efbea5456b325867685dea3dea39d5a
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2014-11-06 21:57:47 +0800

    TL after DP added

--- cf/Bayan_2015/c.c
+++ cf/Bayan_2015/c.c
@@ -22,31 +22,23 @@ char buf[MAXN];                 /*quick IO*/
 
 int area;                       /*area brush covered*/
 int row, col;                   /*brush size*/
-int row1, row2, col1, col2;     /*brush pos*/  
+int row_pos, col_pos;	                /*brush pos*/  
 int m, n;                       /*frame size*/
 int start_row, start_col;       /*start pos*/
 
 #define CLR(vec) memset(vec, 0, sizeof(vec))
 
-#define CHECK_MV(x, y, d, r)    ( (x + d <= m) && (y + r <= n) && (sum[x + d][ y + r] - sum[x + d][y] - sum[x][y + r] + sum[x][y] == (d)*(r)))
-
-int check_valid(int row1, int row2, int col1, int col2){ 
-    
-    int i, j;
-    if(row2 >= m || col2 >= n)
-                return -1;
-
-    for(i = row1; i <= row2; i++)
-        for(j = col1; j <= col2; j++)
-                if(!table[i][j])
-                        return -1;
-    return 0;
-}
+#define CHECK_MV(x, y, d, r)    ( (x + d <= m) && (y + r <= n) && \
+		(sum[x + d][ y + r] - sum[x + d][y] - sum[x][y + r] + sum[x][y] == (d)*(r)))
 
 
-int dfs(int row1, int col1, int left){
+int dfs(int row_pos, int col_pos, int left){
     int col_move;
     int row_move;
+
+    if(0 == CHECK_MV(row_pos, col_pos, row, col)){
+    	return -1;	
+    }
 
     while(1){
 
@@ -54,19 +46,19 @@ int dfs(int row1, int col1, int left){
             return 0;
         }
         
-        for(col_move = 0;  CHECK_MV(row1, col1 , row, col_move +  col); col_move++)
+        for(col_move = 0;  CHECK_MV(row_pos, col_pos , row, col_move +  col); col_move++)
                     ;
 #ifdef DEBUG
-    printf("###############################\n");
+    printf("--------------------------------------------------\n");
 #endif
-        for(row_move = 0;  CHECK_MV(row1, col1, row_move + row, col); row_move++)
+        for(row_move = 0;  CHECK_MV(row_pos, col_pos, row_move + row, col); row_move++)
                     ;
 
         row_move--;
         col_move--;
 
 #ifdef DEBUG
-    printf("row1:%d, col1:%d, col_move:%d, row_move:%d, left:%d\n", row1, col1, col_move, row_move, left);
+    printf("row_pos:%d, col_pos:%d, col_move:%d, row_move:%d, left:%d\n", row_pos, col_pos, col_move, row_move, left);
 #endif
 
         if( !col_move &&!row_move)
@@ -75,10 +67,10 @@ int dfs(int row1, int col1, int left){
             return -1;
 
         if(col_move){
-            col1 += col_move;
+            col_pos += col_move;
             left -= row*col_move;
         }else{
-            row1 += row_move;
+            row_pos += row_move;
             left -= col*row_move;
         }
     }
@@ -112,7 +104,8 @@ int main()
                     }else{
                             table[i][j] = 0;
                     }
-                    sum[i + 1][j + 1] =  sum[i][j + 1] + sum[i + 1][j] - sum[i][j] + (table[i][j]? 1 : 0); /*dp here*/ 
+		    /*dp here*/ 
+                    sum[i + 1][j + 1] =  sum[i][j + 1] + sum[i + 1][j] - sum[i][j] + (table[i][j]? 1 : 0); 
             }
     }
 #ifdef DEBUG
@@ -127,10 +120,7 @@ int main()
         printf("\n");
     }
 #endif
-    row1 = start_row;
-    row2 = start_row;
-    col1 = start_col;
-    col2 = start_col;
+
 #ifdef  DEBUG
     printf("start_row: %d; start_col:%d\n", start_row, start_col);
 #endif
@@ -141,13 +131,8 @@ int main()
 
                 col = area/row;
 
-                row2 = row1 + row -1;
-                col2 = col1 + col -1;
-
-            if(check_valid(row1, row2, col1, col2)< 0)
-                            continue;
 #ifdef DEBUG
-    printf("row:%d , col:%d, row1:%d, row2:%d, col1:%d, col2:%d->>>>\n", row, col, row1, row2, col1, col2);
+    printf("row:%d , col:%d, row_pos:%d, col_pos:%d, ->>>>\n", row, col, row_pos, col_pos);
 #endif
 
             if( 0 == dfs(start_row, start_col, tot_alters - area)){
