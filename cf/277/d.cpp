faafbd1e00a98cf37084f7c62682b195a25fa191
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2014-11-17 14:27:27 +0800

    d code finish

---
+++ cf/277/d.cpp
@@ -0,0 +1,78 @@
+/**
+ * @brief Codeforces Round #277 (Div. 2) d
+ * @file c.c
+ * @author 面码
+ * @created 2014/11/17 15:00
+ * @edited  2014/11/17 15:00
+ * @type dfs dp
+ * @TODO ac
+ *
+ */
+#include <cstdio>
+#include <vector>
+
+#define max(a, b)  ((a) > (b) ? (a) : (b))
+#define min(a, b)  ((a) > (b) ? (b) : (a)) 
+#define abs(a)     ((a) >  0  ? (a) : (0 - (a)))
+
+#define MOD 1000000007
+#define MAXN 2014
+
+using namespace std;
+
+typedef long long int ll;
+
+int d, n;
+int u, v;
+
+int val[MAXN];
+vector<int> tree[MAXN];
+
+/**
+ * @brief dfs and try to add new node
+ * @param[in] root start pos;
+ * @param[in] base  edge start pos
+ * @param[in] curr    edge end pos
+ * @note 
+ *          every tree root hold min val and min index
+ *
+ */
+int dfs(int root, int base, int curr){
+    int ans, to;
+    
+    /*in case of conflict*/
+    if( (val[curr] - val[root] >= d) || ( val[curr] == val[root] && root > curr))   
+            return 0;
+    ans = 1;
+    for(int i = 0; i < tree[curr].size(); i++){
+        to = tree[curr][i];
+        if(to == base)
+            continue;
+        ans = (ll)ans * (1 + dfs(root, curr, to))%MOD;
+    }
+    return ans;
+}
+
+int main(void){
+#ifdef DEBUG
+    freopen("./in",  "r", stdin);
+    freopen("./out", "w", stdout);
+#endif
+    scanf("%d%d", &d, &n);
+    for(int i = 1; i <= n; i++)
+            scanf("%d", &val[i]);
+    for(int i = 1; i < n;  i++){
+            scanf("%d%d", &u, &v);
+            tree[u].push_back(v);
+            tree[v].push_back(u);
+    }
+
+    int ans  = 0;
+    
+    for(int i = 1; i <= n; i++)
+        ans = (ans + dfs(i, 0, i))%MOD;
+    
+    printf("%d\n", ans);
+    return 0;
+}
+
