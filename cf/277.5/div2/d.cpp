7946b962bde599c05ff455bf501a46430e3772c5
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2014-11-19 04:09:09 -0500

    d for div2 in codeforces 277.5 finished and wait to check

---
+++ cf/277.5/div2/d.cpp
@@ -0,0 +1,78 @@
+/**
+ * @brief Codeforces Round #277.5 (Div. 2) d
+ * @file d.cpp
+ * @author 面码
+ * @created 2014/11/19 17:07
+ * @edited  2014/11/19 17:07
+ * @type dfs
+ */
+#include <cstdio>
+#include <cstring>
+#include <stack>
+#include <vector>
+
+#define MAXN 3010   /*max note*/
+#define MAXE 30010  /*max edge*/
+
+#define max(a, b)  ((a) > (b) ? (a) : (b))
+#define min(a, b)  ((a) > (b) ? (b) : (a)) 
+#define abs(a)     ((a) >  0  ? (a) : (0 - (a)))
+#define CLR(vec)   memset(vec, 0, sizeof(vec))
+
+using namespace std;
+
+int ans;
+int n, m;
+int color[MAXN];
+
+vector<int>tree[MAXN];
+stack<int> st;
+
+enum COLOR{
+    WHITE = 0,
+    GRAY,
+    BLACK,
+};
+
+void dfs(int root){
+    int from, to, idx;
+    color[root] = GRAY;
+    st.push(root);
+    while(!st.empty()){
+        from = st.top();
+        st.pop();
+        for(idx = 0; idx < tree[from].size(); idx++){
+            to = tree[from][idx];
+                if(WHITE != color[to]){
+                    if(BLACK == color[to])
+                        ++ans;
+                    return;
+                }else{
+                    color[to] = GRAY;
+                    st.push(to);
+                }
+        }
+        color[from] = BLACK;
+    }
+}
+
+int main()
+{
+    int i, j, k;
+#ifdef DEBUG
+    freopen("./in",  "r", stdin);
+    freopen("./out", "w", stdout);
+#endif
+    scanf("%d%d", &n, &m);
+    for(i = 0; i < m; i++){
+        scanf("%d%d", &j, &k);
+        tree[j].push_back(k);
+    }
+    ans = 0;
+    for(i = 0; i < n; i++){
+        CLR(color);
+        dfs(i);
+    }
+    printf("%d\n", ans);
+    return 0;
+}
