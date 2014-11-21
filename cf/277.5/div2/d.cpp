e042195c4ce558995223e8e57fdfc947c88528f5
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2014-11-21 05:33:55 -0500

    fix bugs

--- cf/277.5/div2/d.cpp
+++ cf/277.5/div2/d.cpp
@@ -2,14 +2,13 @@
  * @brief Codeforces Round #277.5 (Div. 2) d
  * @file d.cpp
  * @author 面码
- * @created 2014/11/19 17:07
- * @edited  2014/11/19 17:07
- * @type dfs brute
- * @TODO test
+ * @created 2014/11/19 18:32
+ * @edited  2014/11/19 18:32
+ * @type brute
+ * #TODO 题意理解错误，更改部分待完成
  */
 #include <cstdio>
 #include <cstring>
-#include <stack>
 #include <vector>
 
 #define MAXN 3010   /*max note*/
@@ -24,46 +23,12 @@ using namespace std;
 
 int ans;
 int n, m;
-int color[MAXN];
-int count[MAXN];
 
 vector<int>tree[MAXN];
-stack<int> st;
-
-enum COLOR{
-    WHITE = 0,
-    GRAY,
-    BLACK,
-};
-
-void dfs(int root){
-    int from, to, idx;
-    color[root] = GRAY;
-    st.push(root);
-    while(!st.empty()){
-        from = st.top();
-        st.pop();
-        for(idx = 0; idx < tree[from].size(); idx++){
-            to = tree[from][idx];
-                if(WHITE != color[to]){
-                    if(BLACK == color[to]){
-                        count[to]++;
-#ifdef DEBUG
-                        printf("Get black in %d\n", to);
-#endif
-                    }
-                }else{
-                    color[to] = GRAY;
-                    st.push(to);
-                }
-        }
-        color[from] = BLACK;
-    }
-}
 
 int main()
 {
-    int i, j, k, tmp;
+    int i, j, k, cnt;
 #ifdef DEBUG
     freopen("./in",  "r", stdin);
     freopen("./out", "w", stdout);
@@ -74,15 +39,17 @@ int main()
         tree[j].push_back(k);
     }
     ans = 0;
-    for(i = 1; i <= n; i++){
-        CLR(color);
-        CLR(count);
-        dfs(i);
-        for(j = 1; j <= n; j++)
-            if(count[j]){
-                ans++;
+    for( i = 1; i <= n; i++)
+        for( j = 1; j <= n; j++){
+            if( i == j)
+                continue;
+            cnt = 0;
+            for(k = 0; k < tree[i].size(); k++){
+                    if(tree[i][k] == j)
+                        cnt++;
             }
-    }
+            ans += cnt ? (cnt * (cnt - 1))>>1 : 0;
+        }
     printf("%d\n", ans);
     return 0;
 }
