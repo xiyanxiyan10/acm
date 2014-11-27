52467daa9e967537818f90c05ba7c4bebd8df999
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2014-11-27 02:30:23 -0500

    b problem for 279 div2 code finished

---
+++ cf/279/div2/b.cpp
@@ -0,0 +1,73 @@
+/**
+ * @brief Codeforces Round #279 (Div. 2) b
+ * @file b.cpp
+ * @author 面码
+ * @created 2014/11/27 15:29
+ * @edited  2014/11/27 15:29
+ * @type 
+ * @note
+ *      1. build 0 <- ? -> a <- ? -> b ->0
+ *      2. get ans
+ *
+ * @TODO wait to test
+ */
+#include <cstdio>
+#include <stack>
+#include <cstring>
+
+using namespace std;
+
+#define max(a, b)  ((a) > (b) ? (a) : (b))
+#define min(a, b)  ((a) > (b) ? (b) : (a)) 
+#define abs(a)     ((a) >  0  ? (a) : (0 - (a)))
+#define CLR(vec)   memset(vec, 0, sizeof(vec))
+
+#define MAXN 1000010
+
+int n;
+int cnt;
+
+/*stand from left to right*/
+int ans[MAXN];
+int lft[MAXN];
+int rht[MAXN];
+
+int i;
+int left, right;
+
+int main(void){
+#ifdef DEBUG
+    freopen("./in",  "r", stdin);
+    freopen("./out", "w", stdout);
+#endif
+    scanf("%d", &n);
+    for(i = 0; i < n ; i++){
+        scanf("%d%d", left, right);
+        lft[right] = left;
+        rht[left]  = right;
+    }
+
+    /*2 need spicial solve*/
+    if(n == 2){
+        printf("%d %d\n", lft[0], rht[0]);
+        return 0;
+    }
+
+    /*get header*/
+    right = rht[0];     
+    while(right != 0)
+        ans[cnt++] = right;
+    
+    /*output header*/
+    printf("%d ", lft[ans[cnt]]);
+    
+    for(i = 0; i < cnt - 1; i++)
+        printf("%d ", rht[ans[cnt]]);
+    
+    printf("%d ", rht[ans[cnt]]);
+    
+    return 0;
+}
+
+
+
