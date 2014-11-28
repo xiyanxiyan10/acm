ae136ac8cbe77c33de2e170089cb5a5de8b76196
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2014-11-28 17:09:15 +0800

    c solve fail with big num

---
+++ cf/279/div2/c.cpp
@@ -0,0 +1,86 @@
+/**
+ * @brief Codeforces Round #279 (Div. 2) c
+ * @file c.cpp
+ * @author 面码
+ * @created 2014/11/28 17:06
+ * @edited  2014/11/28 17:06
+ * @type math
+ * @TODO check all rht big num
+ * @note
+ */
+#include <fstream>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+#define max(a, b)  ((a) > (b) ? (a) : (b))
+#define min(a, b)  ((a) > (b) ? (b) : (a)) 
+#define abs(a)     ((a) >  0  ? (a) : (0 - (a)))
+#define CLR(vec)   memset(vec, 0, sizeof(vec))
+
+#define MAXN 1000010
+
+#ifdef DEBUG
+ifstream in;
+ofstrream out;
+#define CIN in
+#define COUT out
+#else
+#define CIN cin
+#define COUT cout
+#endif
+
+int num[MAXN];           /*store big num */
+int lft_record[MAXN];    /*division pos record*/
+int rht_record[MAXN];    /*division pos record*/
+
+int i;
+int lft, rht;
+int size;
+int val;
+int base;
+int tmp;
+string str;
+
+int main(void){
+#ifdef DEBUG
+    CIN.open("./in",  ios::in);
+    COUT.open("./out",  ios::out);
+#endif
+    CIN >> str;
+    CIN >> lft >> rht;
+
+    size = str.size();
+
+    /*check str*/
+    if('0' != str[0] && size < 2){
+        COUT << "NO\n";
+        return 0;
+    }
+
+    for(int i = 0; i < size; i++)
+            num[i] = str[i] - '0';
+    
+    /*calculate all lft big num division */
+    val = 0;
+    for(int pos = 0; pos < size; pos++){ 
+        val = val*10 + num[pos];
+        if(val < lft)                       /*too small, go on*/
+                continue;
+
+        tmp = val%lft;
+        if(!tmp)
+            lft_record[pos + 1] = 1;        /*record lft string len*/
+    }
+
+    /*calculate all rht big num division*/
+    for(int pos = 1; pos < size; pos++){ 
+           
+
+    }
+    return 0;
+}
+
+
+
