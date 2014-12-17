262618e8cec33cb3cb0500414c822aff3bc4a48b
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2014-12-17 03:52:24 -0500

    code finished

--- livearchive/3401.cpp
+++ livearchive/3401.cpp
@@ -2,8 +2,8 @@
  * @brief live archive 3401
  * @file 3401.cpp
  * @author mianma
- * @created 2014/12/17 15:09
- * @edited  2014/12/17 15:09
+ * @created 2014/12/17 16:51
+ * @edited  2014/12/17 16:51
  * @type 
  * @note
  */
@@ -39,7 +39,6 @@ int lft[] = { 1, 5, 2, 3, 0, 4};
 /*if cude rot up,   0, 1, 2, 3, 4, 5 -> 3, 1, 0, 5, 4,  2*/
 int up[] =  { 3, 1, 0, 5, 4, 2};
 
-
 /*rot*/
 void rot(int *dir, int *base){
     int tmp[MAXV];
@@ -48,8 +47,12 @@ void rot(int *dir, int *base){
             base[i] = tmp[dir[i]];
 }
 
+vector<string> db;
 int dice[MAXB][MAXV];
 int table[MAXN][MAXV];
+int status[MAXN];
+int n;
+int ans;
 
 /*brute all status*/
 void brute(){
@@ -58,14 +61,8 @@ void brute(){
     int base[MAXV] = { 0, 1, 2, 3, 4, 5};
     for(int i = 0; i < MAXV; i++){
         memcpy(tmp, base, sizeof(tmp));
-        if(0 == i){ 
-            rot(up, tmp);  
-        
-        }
-        if(1 == i){ 
-            rot(lft, tmp); 
-            rot(up, tmp); 
-        }
+        if(0 == i){ rot(up, tmp);  }
+        if(1 == i){ rot(lft, tmp); rot(up, tmp); }
         if(2 == i){ ;}
         if(3 == i){ rot(up, tmp); rot(up, tmp); }
         if(4 == i){ rot(lft, tmp); rot(lft, tmp); rot(lft, tmp); rot(up, tmp); }
@@ -77,8 +74,6 @@ void brute(){
         }
     }
 }
-
-vector<string> db;
 
 void db_init(){
     db.clear();
@@ -93,16 +88,55 @@ int db_add(string color){
     return i;
 }
 
+/*try to get better answer*/
+void check_ans()
+{
+    /*counter difference color*/
+    int counter[MAXB];
+    int tot, curr_max;
+    for(int i = 0; i < MAXV; i++){
+        CLR(counter);
+        curr_max = -1;
+        for(int j = 0; j < n; j++){
+            curr_max = max(curr_max, ++counter[dice[status[j]][i]]);
+        }
+        tot += MAXV - curr_max;
+    }
+    ans = min(tot, ans);
+}
+
+void dfs(int d){
+    if(d == n )
+        check_ans();
+    else{
+        for(int i = 0; i <= 24; i++){
+            status[d] = i;
+            dfs(d + 1);
+        }
+    }   
+}
+
 int main(void){
     ios_base::sync_with_stdio(0);
 #ifdef DEBUG
     CIN.open("./in",  ios::in);
     COUT.open("./out",  ios::out);
 #endif
+    string color;
     /*init status table*/
     brute();
-    /*read input and solve problem*/
-
+    while(CIN >> n && n){
+    /*init color db*/
+        db_init();
+        for(int i = 0; i < n; i ++)
+            for(int j = 0; j < MAXV; j++){
+                CIN >> color;
+                table[i][j] = db_add(color);
+            }
+        ans = n*MAXV;
+        status[0] = 0;
+        dfs(1);
+    }
     return 0;
 }
 
