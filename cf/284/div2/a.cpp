52ad9a97c6a3240df228a495fa5416ffd028c819
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2014-12-25 23:59:31 -0500

    a b ac

---
+++ cf/284/div2/a.cpp
@@ -0,0 +1,58 @@
+/**
+ * @brief Codeforces Round #284 (Div. 2) a
+ * @file a.cpp
+ * @author mianma
+ * @created 2014/12/26 11:51
+ * @edited  2014/12/18 11:51
+ * @type brute
+ * @note
+ */
+#include <fstream>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+#define max(a, b)  ((a) > (b) ? (a) : (b))
+#define min(a, b)  ((a) > (b) ? (b) : (a)) 
+#define abs(a)     ((a) >  0  ? (a) : (0 - (a)))
+#define CLR(vec)   memset(vec, 0, sizeof(vec))
+
+#ifdef DEBUG
+ifstream in;
+ofstream out;
+#define CIN in
+#define COUT out
+#else
+#define CIN cin
+#define COUT cout
+#endif
+
+#define MAXN 50
+#define MAXV 1e5
+
+int n, x;
+int curr, ans, lft, rht;
+
+int main(void){
+    ios_base::sync_with_stdio(0);
+#ifdef DEBUG
+    CIN.open("./in",  ios::in);
+    COUT.open("./out",  ios::out);
+#endif
+    CIN >> n >> x;
+    int curr = 1;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        CIN >> lft >> rht;
+        if(curr > lft)
+            lft = curr;
+        ans += (lft - curr)%x + rht - lft + 1;
+        curr = rht + 1;
+    }
+    COUT << ans  << "\n";
+    return 0;
+}
+
+
+
---
+++ cf/284/div2/b.cpp
@@ -0,0 +1,60 @@
+/**
+ * @brief Codeforces Round #284 (Div. 2) b
+ * @file b.cpp
+ * @author mianma
+ * @created 2014/12/26 11:51
+ * @edited  2014/12/18 11:51
+ * @type brute
+ * @note
+ */
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+#define max(a, b)  ((a) > (b) ? (a) : (b))
+#define min(a, b)  ((a) > (b) ? (b) : (a)) 
+#define abs(a)     ((a) >  0  ? (a) : (0 - (a)))
+#define CLR(vec)   memset(vec, 0, sizeof(vec))
+#define PI         acos(-1.0)
+
+#ifdef DEBUG
+ifstream in;
+ofstream out;
+#define CIN in
+#define COUT out
+#else
+#define CIN cin
+#define COUT cout
+#endif
+
+int n, m;
+string lft, rht, str;
+map<string, string> table;
+
+int main(void){
+    ios_base::sync_with_stdio(0);
+#ifdef DEBUG
+    CIN.open("./in",  ios::in);
+    COUT.open("./out",  ios::out);
+#endif
+    CIN >> n >> m;
+    for(int i = 0; i < m; i++){
+        CIN >> lft >> rht;
+        if(lft.size() <= rht.size())
+                rht = lft;
+        table[lft] = rht;
+    }
+    for(int i = 0; i < n; i++){
+        CIN >> str;
+        COUT << table[str];
+        COUT << (i == n - 1 ? "\n":" ");
+    }
+    return 0;
+}
+
+
+
