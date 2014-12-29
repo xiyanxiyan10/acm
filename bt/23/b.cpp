e812e624c140b170b4542bc40109988a42bdd938
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2014-12-29 21:41:09 +0800

    fix bug

--- bt/23/b.cpp
+++ bt/23/b.cpp
@@ -63,8 +63,9 @@ int  main(void)
             if(i >= 3 && pp != curr)
                     record[i] = 1;
         }
-        
-        for(int i = 1; i <= n; i++)
+
+	dp[0][1][1] = 1;
+        for(int i = 2; i <= n; i++)
             for(int j = 1; j <= i; j++){
                 if(record[i]){
                     tmp = 0;
