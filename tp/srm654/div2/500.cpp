a09c30b88a0c4551606aacc7fdf5249afef6f9f1
xiyanxiyan10 <xiyanxiyan10@gmail.com>
2015-03-30 10:28:24 +0800

    500 for srm654 pass

--- tp/srm654/div2/500.cpp
+++ tp/srm654/div2/500.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <algorithm>
 #include <cstring>
+#include <iostream>
 
 
 #define CLR(vec)  memset(vec, 0, sizeof(vec))
@@ -18,10 +19,14 @@ vector<int> nums;
 int rec[MAXN];
 
 void dfs(int curr, int father, int key){
-	if(curr == key){
-		rec[key] = 1;
-		return;
-	}
+    if(rec[curr])
+        return;
+
+    if(curr == key){
+	rec[key] = 0x1;    
+	return;
+    }
+
     for(int i = 0; i < gf[curr].size(); i++){
         int next = gf[curr][i];
         if(next == father || rec[next]){
@@ -29,33 +34,46 @@ void dfs(int curr, int father, int key){
         }
         dfs(next, curr, key);
     }
-	return;
+    return;
 }
 
 
 class OneEntrance {
 	public:
 	int count(vector <int> a, vector <int> b, int s){
-		for(int i = 0; i < a.size(); i++){
-            gf[a[i]].push_back(b[i]);
-            gf[b[i]].push_back(a[i]);
-        }
-		for(int i = 0; i <= a.size(); i++)
-				nums.push_back(i);
-		int ans = 0;
-		do{
-				CLR(rec);
-				int success = 1;
-				for(int i = 0; i < nums.size(); i++){
-					int key = nums[i];
-					dfs(s, s, key);
-					if(0 == rec[key]){
-						success = 0;
-						break;
-					}
-				}
-				if(success) ++ans;
-		}while(next_permutation(nums.begin(), nums.end()));
-		return ans;
+	    for(int i = 0; i < a.size(); i++){
+                gf[a[i]].push_back(b[i]);
+                gf[b[i]].push_back(a[i]);
+            }
+
+	    for(int i = 0; i <= a.size(); i++)
+		nums.push_back(i);
+		
+	    int ans = 0;
+	    do{
+#ifdef  DEBUG
+                cout<< "permuation:\n";
+                for(vector<int>::iterator iter = nums.begin(); iter != nums.end(); ++iter)
+                    cout<< *iter << " ";
+                cout << endl;
+#endif
+                    CLR(rec);
+		    int success = 1;
+		    for(int i = 0; i < nums.size(); i++){
+			    int key = nums[i];
+			    dfs(s, s, key);
+			    if(0 == rec[key]){
+#ifdef DEBUG
+                                cout<< key << " check fail\n";
+#endif
+				success = 0;
+				break;
+			    }
+		    }
+		    if(success) ++ans;
+	    }while(next_permutation(nums.begin(), nums.end()));
+		
+	    return ans;
 	}
 };
+
